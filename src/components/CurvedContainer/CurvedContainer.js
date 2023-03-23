4dd2eebc8ac7f11ffb28885d1462005126e771fa
shhyaaam <shyamkrzn@gmail.com>
2023-03-23 13:45:44 +0530

    Added hover effect to events.

--- src/components/CurvedContainer/CurvedContainer.js
+++ src/components/CurvedContainer/CurvedContainer.js
@@ -13,7 +13,7 @@ const CurvedContainer = ({ logo, clip, className, to, text, workshop, onClick })
     <div className={`w-10/12 md:w-1/4 h-1/4 ${className}`}>
       <div className={`curved_container w-full h-full ${clip && 'clip_path'}`}>
         <div className={`inner ${clip && 'clip_path'}`}>
-          {onClick ? <img src={logo} className="w-full h-full" alt="" onClick={onClick}/>: <Link to={text ? '/error' : getRedirection(workshop, to)}><img src={logo} className="w-full h-full" alt="" /></Link>}
+          {onClick ? <img src={logo} className="w-full h-full" alt="" onClick={onClick} /> : <Link to={text ? '/error' : getRedirection(workshop, to)}><img src={logo} className="w-full h-full" alt="" /></Link>}
         </div>
       </div>
       {text && <h3 className='text-white mt-4 md:mt-8 font-chakra font-bold text-center uppercase text-2xl'>{text}</h3>}
