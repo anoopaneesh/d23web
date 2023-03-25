0f10070af8e666af366f6c72f50965a0a88ddac0
anoopaneesh808 <anoopaneesh808@gmail.com>
2023-03-25 15:37:04 +0530

    added contact details in accomodation page

--- src/pages/Accomodation/Accomodation.js
+++ src/pages/Accomodation/Accomodation.js
@@ -12,7 +12,7 @@ const Accomodation = () => {
     return (
         <div className='h-screen font-chakra text-white flex flex-col'>
             <Navbar />
-            <div className='flex-1 flex flex-col justify-center items-center w-11/12 md:w-2/3 m-auto space-y-10'>
+            <div className='flex-1 flex flex-col justify-center items-center w-full px-4 md:px-0 md:w-2/3 m-auto space-y-10'>
                 <h1 className='text-5xl md:text-6xl font-bold text-heading'>Accomodations</h1>
                 <p className='text-justify'>During the event days, attendees have
                     the opportunity to stay in paid hostel
@@ -28,6 +28,11 @@ const Accomodation = () => {
                     and ensure that they have a
                     comfortable and secure place to stay
                     throughout the duration of the event.</p>
+                <div className='w-full'>
+                    <p className='underline'>Contact</p>
+                    <p>Arjun : 9400056465</p>
+                    <p>Rasica : 9188074576</p>
+                </div>
                 <a href="https://www.yepdesk.com/accomodation" target="_blank" rel="noreferrer" className='text-center'><button className=' bg-[#36fdfd] proshow_button font-chakra' type="button" style={{
                     background
                         : `linear-gradient(45deg, transparent 5%, var(--heading) 5%)`, color: 'black', boxShadow: `6px 0 0 var(--c_red)`,
