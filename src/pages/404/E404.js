fef8e8da247a1dcea51998276a67fff419931fa9
shhyaaam <shyamkrzn@gmail.com>
2023-03-31 00:34:43 +0530

    Changed Hero

--- src/pages/404/E404.js
+++ src/pages/404/E404.js
@@ -1,10 +1,29 @@
+import { text } from '@fortawesome/fontawesome-svg-core';
 import React from 'react'
 import './E404.css'
 
 
 
 
+
 const E404 = () => {
+
+    var x = "";
+    function find(y) {
+        if (x == "098765") { calcPath('xxx') }
+        x += y;
+        console.log(x);
+    }
+
+    function calcPath(str) {
+        var string = "";
+        for (var i = 0; i < str.length; i++) {
+            var temp = str.charAt(i);
+        }
+        console.log(string);
+        console.log("765ghf");
+        return string;
+    }
 
     return (
         <div class="text_container comming_soon error_body">
@@ -19,16 +38,16 @@ const E404 = () => {
                 <a href="https://www.yepdesk.com/profile/dyuksha" className='text-center' target="_blank"><button className='button bg-c_red font-chakra' type="button">VIEW GALLERY</button></a>
             </div>
             <div className='flex space-x-2 pt-20 item'>
-                <div id='0' className='p_btn'>0</div>
-                <div id='1' className='p_btn'>1</div>
-                <div id='2' className='p_btn'>2</div>
-                <div id='3' className='p_btn'>3</div>
-                <div id='4' className='p_btn'>4</div>
-                <div id='5' className='p_btn'>5</div>
-                <div id='6' className='p_btn'>6</div>
-                <div id='7' className='p_btn'>7</div>
-                <div id='8' className='p_btn'>8</div>
-                <div id='9' className='p_btn'>9</div>
+                <div id='0' onClick={() => calcPath("abc")} className='p_btn'>0</div>
+                <div id='1' onClick={() => find('1')} className='p_btn'>1</div>
+                <div id='2' onClick={() => find('2')} className='p_btn'>2</div>
+                <div id='3' onClick={() => find('3')} className='p_btn'>3</div>
+                <div id='4' onClick={() => find('4')} className='p_btn'>4</div>
+                <div id='5' onClick={() => find('5')} className='p_btn'>5</div>
+                <div id='6' onClick={() => find('6')} className='p_btn'>6</div>
+                <div id='7' onClick={() => find('7')} className='p_btn'>7</div>
+                <div id='8' onClick={() => find('8')} className='p_btn'>8</div>
+                <div id='9' onClick={() => find('9')} className='p_btn'>9</div>
             </div>
         </div>
     )
